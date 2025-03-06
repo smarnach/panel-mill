39343e862c0709dc635e29709fa98fe9fe4eaaa1
Sven Marnach <sven@mozilla.com>
2025-03-06 16:46:46 +0100

    Add upload metrics to Tecken dashboard.

--- dashboards/tecken.py
+++ dashboards/tecken.py
@@ -1,6 +1,13 @@
 from panel_mill.dashboard import Dashboard
+from panel_mill.panels.base import Timeseries
 from panel_mill.panels.gclb import GCLBMixin
 from panel_mill.panels.kubernetes import KubernetesMixin
+from panel_mill.promql import LabelFilters
+
+from grafana_foundation_sdk.builders.dashboard import Row
+from grafana_foundation_sdk.models.dashboard import DynamicConfigValue
+
+from typing import Self
 
 
 class TeckenDashboard(GCLBMixin, KubernetesMixin, Dashboard):
@@ -20,4 +27,65 @@ class TeckenDashboard(GCLBMixin, KubernetesMixin, Dashboard):
                 volume_names=["uploads"],
                 container="tecken",
             )
+            .upload_metrics()
         )
+
+    def upload_metrics(self) -> Self:
+        filters = LabelFilters('namespace="$namespace"')
+        (
+            self.with_row(Row("Upload metrics"))
+            .with_panel(
+                Timeseries()
+                .title("KEYPANEL: Upload request times")
+                .with_summary_quantile_target("tecken_upload_archive", filters)
+            )
+            .with_panel(
+                Timeseries()
+                .title("Count of uploads")
+                .with_count_target("tecken_upload_uploads", filters, by="try")
+                .override_by_name(
+                    "False", [DynamicConfigValue("displayName", "regular")]
+                )
+                .override_by_name("True", [DynamicConfigValue("displayName", "try")])
+            )
+            .with_panel(
+                Timeseries()
+                .title("File uploads and skips")
+                .with_count_target(
+                    "tecken_upload_file_upload_upload", filters, legend_format="uploads"
+                )
+                .with_count_target(
+                    "tecken_upload_file_upload_skip", filters, legend_format="skips"
+                )
+            )
+            .with_panel(
+                Timeseries()
+                .title("Skip uploads early counts")
+                .with_count_target(
+                    "tecken_upload_skip_early_compressed",
+                    filters,
+                    legend_format="compressed",
+                )
+                .with_count_target(
+                    "tecken_upload_skip_early_uncompressed",
+                    filters,
+                    legend_format="uncompressed",
+                )
+            )
+            .with_panel(
+                Timeseries()
+                .title("Time to  upload individual symbols file to GCS")
+                .with_summary_quantile_target("tecken_upload_file_upload", filters)
+            )
+            .with_panel(
+                Timeseries()
+                .title("Time to Upload by Download URL")
+                .with_summary_quantile_target("tecken_upload_download_by_url", filters)
+            )
+            .with_panel(
+                Timeseries()
+                .title("Dump and extract zip file to disk")
+                .with_summary_quantile_target("tecken_upload_dump_and_extract", filters)
+            )
+        )
+        return self
