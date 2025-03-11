0e3106039d8a27d29bec2694e22c130e2f0df651
Sven Marnach <sven@mozilla.com>
2025-03-11 16:51:47 +0100

    Add GCLB all 500s panel.

--- panel_mill/panels/gclb.py
+++ panel_mill/panels/gclb.py
@@ -10,10 +10,9 @@ from typing import Self
 
 class GCLBMixin(Dashboard):
     def _gclb_request_rate_by_status_code(
-        self, filters: LabelFilters, title: str, operator: str
+        self, filters: LabelFilters, title: str
     ) -> Timeseries:
         metric = "loadbalancing_googleapis_com:https_request_count"
-        filters = filters + f'backend_type{operator}"FRONTEND_5XX"'
         query = f"sum by(response_code) (rate({metric}{filters}[$__rate_interval]))"
         return (
             self.stacked_rps_timeseries_panel()
@@ -23,15 +22,22 @@ class GCLBMixin(Dashboard):
 
     def gclb_request_rate_by_status_code(self, filters: LabelFilters) -> Timeseries:
         return self._gclb_request_rate_by_status_code(
-            filters, "KEYPANEL: Request rate by status code", "!="
+            filters + 'backend_type!="FRONTEND_5XX"',
+            "KEYPANEL: Request rate by status code",
         )
 
     def gclb_request_rate_lb_5xx(self, filters: LabelFilters) -> Timeseries:
         return self._gclb_request_rate_by_status_code(
-            filters, "Load balancer 500s", "="
+            filters + 'backend_type="FRONTEND_5XX"', "Load balancer 500s"
         ).description(
             "These are 500s emitted by the load balancer without sending the "
             "request to any backend. Some of these are outside of our control."
+        )
+
+    def gclb_request_rate_all_5xx(self, filters: LabelFilters) -> Timeseries:
+        return self._gclb_request_rate_by_status_code(
+            filters + 'response_code_class="500"',
+            "All 500s (load balancer and backend)",
         )
 
     def gclb_backend_latency(self, filters: LabelFilters) -> Timeseries:
@@ -54,5 +60,6 @@ class GCLBMixin(Dashboard):
             self.with_row(Row("GCLB"))
             .with_panel(self.gclb_request_rate_by_status_code(filters))
             .with_panel(self.gclb_request_rate_lb_5xx(filters))
+            .with_panel(self.gclb_request_rate_all_5xx(filters))
             .with_panel(self.gclb_backend_latency(filters))
         )
