460f0ed895d67bec7906ca0207f532c1ee1bc90b
Sven Marnach <sven@mozilla.com>
2025-03-13 10:19:25 +0100

    Move JSON encoder out of loop.

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ ALL_DASHBOARDS = {
 }
 
 def main():
+    encoder = JSONEncoder(sort_keys=True, indent=2)
     build_path = Path("build")
     for dashboard_class, file_name in ALL_DASHBOARDS.items():
         module_path, class_name = dashboard_class.rsplit(".", 1)
         module = import_module(module_path)
         dashboard = getattr(module, class_name)().build()
-        encoder = JSONEncoder(sort_keys=True, indent=2)
         with (build_path / file_name).open("w") as f:
             f.write(encoder.encode(dashboard))
 
